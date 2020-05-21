654afcd7c3d74c021e4d21fd9384ef5706b96397
koogiyun <gyu523@naver.com>
2020-05-21 17:00:33 +0900

    추가기능 추가

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,16 +1,30 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./CreateForm.css";
 class CreateForm extends React.Component {
-	showInputValue = e => {
-		const { previousElementSibling } = e.target;
-		this.props.onCreate(previousElementSibling.value);
-		previousElementSibling.value = "";
+	state = {
+		input: ""
+	};
+
+	onChange = e => {
+		this.setState({
+			input: e.target.value
+		});
+	};
+
+	onCreate = () => {
+		this.props.onCreate(this.state.input);
+		this.setState({ input: "" });
 	};
 	render() {
 		return (
 			<div className="create-form">
-				<input className="todo" type="text"></input>
-				<button className="btn-create" onClick={this.showInputValue}>
+				<input
+					className="todo"
+					type="text"
+					onChange={this.onChange}
+					value={this.state.input}
+				></input>
+				<button className="btn-create" onClick={this.onCreate}>
 					추가
 				</button>
 			</div>
